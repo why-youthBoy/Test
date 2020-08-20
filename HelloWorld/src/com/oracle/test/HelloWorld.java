8b3d69647450d7f6a0e3fc47bac03ebfc9a7fb11
why <1668011752@qq.com>
2020-08-20 09:44:34 +0800

    first

---
+++ HelloWorld/src/com/oracle/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.oracle.test;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+        System.out.println("why");
+    }
+}
